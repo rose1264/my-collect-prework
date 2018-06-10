4342782055f873de8ec680bd94c0b4a82eda7413
Shumei Huang <rose1264@gmail.com>
2018-06-10 03:59:56 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    newArray = []
+    while i < array.length
+      newArray.push(yield array[i])
+      i += 1
+    end
+    array
+  else
+    "Hey! No block was given!"
+  end
+end
